29ee94eede044d369267bbd2629a4d740f2cd484
Zeeshan Alam <zeeshantechnical9128@gmail.com>
2021-12-17 16:00:47 +0530

    Add get_string

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,23 @@
 use std::io;
 use std::io::Write;
 
+pub fn get_string(q: &str) -> String {
+     loop {
+        print!("{}", q);
+        io::stdout().flush().unwrap();
+    
+        let mut str = String::new();
+    
+        io::stdin().read_line(&mut str).ok();
+        let string = str.trim().parse();
+        let string = match string {
+            Ok(n) => n,
+            Err(_) => continue,
+        };
+        return string
+    }
+}
+
 pub fn get_int(q: &str) -> i32 {
     loop {
         print!("{}", q);
@@ -17,3 +34,4 @@ pub fn get_int(q: &str) -> i32 {
         return num
     }
 }
+
